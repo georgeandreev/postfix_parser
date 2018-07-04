19c2a1d758275cf33ebee0d78e780ee2180efcf8
georgeandreev <georgeandreev6@gmail.com>
2018-07-04 20:13:11 +0500

    date splitter
    Работа программы:
    1. Генерация лога, каждая новая запись добавляется в случайный интервал от 1 до 10 минут.
    2. Реализация бинарного поиска.
    Нижняя граница изначальна равна нулю, а верхняя - удвоенному среднему количеству записей в логе за месяц.
    Каждый шаг цикла сдвигаем верхнюю или нижнюю границу. Если натыкаемся на две подряд идущие даты, у которых разный месяц(граница месяца), то добавляем в массив индексов стартовую нижнюю границу и текущий индекс(или текущий индекс + 1), затем обновляем нижнюю и верхнюю границу, обновляем стартовую нижнюю границу.

--- date_splitter.py
+++ date_splitter.py
@@ -5,45 +5,52 @@ from random import randint
 log_arr = []
 i = 0
 startdate = datetime(2018, 1, 1)
-while i < 600000:
+while i < 500000:
 	log_arr.append((startdate + timedelta(minutes=i)).strftime("%Y-%m-%d %H:%M:%S"))
 	i += randint(1, 10)
 enddate = datetime.strptime(log_arr[-1], '%Y-%m-%d %H:%M:%S')
 add_const = int(len(log_arr) / ((enddate - startdate).days / 30)) * 2
 
 
-# Pair (start, end)
 indexes = []
 start_index = 0
 start = 0
-end = add_const
+end_index = add_const
 last_index = len(log_arr)
-print(end, last_index)
-# exit()
-# Binary search
+
 cur_month = startdate.month
 while True:
-	cur_index = min(int((start_index + end) / 2), last_index)
+
+	cur_index = min(int((start_index + end_index) / 2), last_index)
 	if cur_index == last_index:
 		indexes.append((start_index, last_index))
 		break
 	curdate = datetime.strptime(log_arr[cur_index], '%Y-%m-%d %H:%M:%S')
 	nextdate = datetime.strptime(log_arr[cur_index + 1], '%Y-%m-%d %H:%M:%S')
 	previousdate = datetime.strptime(log_arr[cur_index - 1], '%Y-%m-%d %H:%M:%S')
-	#print(curdate, previousdate, nextdate)
-	#exit()
-	#print(curdate.month, previousdate.month, nextdate.month)
-	#exit()
-	if curdate.month != cur_month:
-		cur_month += 1
+	
+	if curdate.month > previousdate.month or curdate.year > previousdate.year:
 		indexes.append((start, cur_index))
-		start_index = cur_index + 1
+		start = cur_index
+		start_index = cur_index
+		end_index = start_index + add_const
+		cur_month += 1
+		cur_month = max(1, cur_month % 13)
+		continue
+
+	if curdate.month < nextdate.month or curdate.year < nextdate.year:
+		indexes.append((start, cur_index))
 		start = cur_index + 1
-		end = start_index + add_const
+		start_index = cur_index + 1
+		end_index = start_index + add_const
+		cur_month += 1
+		cur_month = max(1, cur_month % 13)
+		continue
+		
+	if curdate.month != cur_month:
+		end_index = cur_index
 	else:
 		start_index = cur_index
-	#print(start, end)
-	#print(log_arr[start])
-	#exit()
+	
 for item in indexes:
 	print(item)
